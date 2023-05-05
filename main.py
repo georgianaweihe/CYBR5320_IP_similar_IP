a51156731ca58afab753e36ddabb305d98e7ab0a
georgianaweihe <georgianaweihe@users.noreply.github.com>
2023-05-05 10:05:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-Python 3.10.9 (v3.10.9:1dd9be6584, Dec  6 2022, 14:37:36) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 import csv
 import argparse 
 import pandas as pd
 import numpy as np
 from collections import defaultdict
 from scipy.spatial import distance
+from numpy.linalg import eig
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--inputfile', type=str, required=True, help="The file with the features and labels.")
@@ -13,52 +12,79 @@ parser.add_argument('--inputfile', type=str, required=True, help="The file with 
 FLAGS = parser.parse_args()
 
 my_df = pd.read_csv(FLAGS.inputfile) #read in file as pandas df
+print ("Reading input file..." )
 
 my_dict = {} #create a dictionary
+print ("Creating dictionary...")
 
 feature_names = ["f1","f2","f3","f4","f5","f6","f7","f8","f9","f10"]
 
+# Mapping from ip address to integer id
+ip2int = {}
+numips = 0
 for index, row in my_df.iterrows():
     src_ip = row.loc["src"]
-    for name in feature_names:
+    if src_ip not in ip2int:
+        ip2int[src_ip] = numips
+        numips += 1
+
+print ("Loop 1 complete" )
+
+for index, row in my_df.iterrows():
+    src_ip = row.loc["src"] #call the source IP
+    for name in feature_names: 
         feature = row.loc[name]
-        print("feature is ",feature)
-...         if src_ip not in my_dict:
-...             my_dict[src_ip] = {}
-...         if name not in my_dict[src_ip]:
-...             my_dict[src_ip][name] = []
-...         my_dict[src_ip][name].append(feature)
-...         print("for source IP ",src_ip," and feature ",name)
-...         print(my_dict[src_ip][name])
-... 
-... dict_of_numpyarrays = {}
-... arr0 = []
-... arr1 = []
-... arr2 = []
-... 
-... #for i in my_dict:
-... #    for j in my_dict:
-... #            num_ips = len(my_dict)
-... #            my_array = np.zeros((num_ips,num_ips))
-... #            dict_of_numpyarrays[i][j] = my_array
-... 
-... for name in feature_names:
-...     num_ips = len(my_dict)
-...     my_array = np.zeros((num_ips,num_ips))
-...     dict_of_numpyarrays[name] = my_array
-... 
-... for name in feature_names:
-...     for ip1 in my_dict:
-...         print("this is ip1 in my_dict:",ip1)
-...         for ip2 in my_dict:
-...             print("this is ip2 in my_dict:",ip2)
-...             l1 = my_dict[ip1][name]
-...             l2 = my_dict[ip2][name]
-...             print("this is l1:",l1)
-...             print("this is l2:",l2)
-...             score = distance.jensenshannon(l1,l2)
-...             print("the JS score is",score)
-... 
-...             print("this is name:",name)
-...             print("this is ip1:",ip1)
-...             print("this is ip2:",ip2)
+        if src_ip not in my_dict: #if the IP is new, create an empty dictionary 
+            my_dict[src_ip] = {}
+        if name not in my_dict[src_ip]: #if the IP exists in the dict, create an array for the feature
+            my_dict[src_ip][name] = []
+        my_dict[src_ip][name].append(feature)
+
+print ("Loop 2 complete" )
+
+dict_of_numpyarrays = {}
+
+# create a dictionary of ten arrays (key value = feature name)
+for name in feature_names:
+    num_ips = len(my_dict)
+    my_array = np.zeros((num_ips,num_ips))
+    dict_of_numpyarrays[name] = my_array
+
+print ("Loop 3 complete" )
+
+#initialize a counter
+f = 0
+file = open("ips-similarities.txt", "w")
+file.close()
+for name in feature_names:
+    print ("Analyzing feature = ", f, "...")
+    g = 0
+    for ip1 in my_dict:
+        ip1_int = ip2int[ip1]
+        x = 0
+        for ip2 in my_dict:
+            print ("Analyzing ip number ", g, " against ip number ", x)
+            l1 = my_dict[ip1][name]
+            l2 = my_dict[ip2][name]
+            # Create histogram, bins
+            hist, bin_edges = np.histogram(l1 + l2)
+            hist1, _ = np.histogram(l1, bins=bin_edges)
+            hist2, _ = np.histogram(l2, bins=bin_edges)
+            score = 1 - distance.jensenshannon(hist1, hist2)
+            # to create sparse matrix (optional): if score <= some threshold, then set score = 0
+            # Translate from ip address to integer identifier
+            ip2_int = ip2int[ip2]
+            dict_of_numpyarrays[name][ip1_int][ip2_int] = score
+            x += 1
+        g += 1
+    f +=1
+
+print ("Loop 4 complete" )
+
+for name in dict_of_numpyarrays:
+    w,v = eig(dict_of_numpyarrays[name])
+    print ("E-value for ", name, ":", w)
+    print("E-vector for ", name, ":", v)
+    np.savetxt(name + ".txt", dict_of_numpyarrays[name], fmt='%.5f', delimiter=',')
+
+print ("Job complete")
